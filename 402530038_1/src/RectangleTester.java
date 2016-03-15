86f56f397003ebea8924a03fa4d741ea04481d91
HanselHu <apologize0616@gmail.com>
2016-03-15 15:35:03 +0800

    作業批改完成!

--- 402530038_1/src/RectangleTester.java
+++ 402530038_1/src/RectangleTester.java
@@ -1,50 +1,51 @@
-class Rectangle
-{
-	double x,y,Height,Width;
-	
-	Rectangle()
-	{
+/*
+ * [A]100
+ * [TA's advise]
+ * 1.Good Job.
+ * */
+
+class Rectangle {
+	double x, y, Height, Width;
+
+	Rectangle() {
+		//this(-1,-1,-1,-1);//加上這行會讓你的程式更好.
 	}
-	
-	Rectangle(double  _x ,double  _y ,double  _Height , double  _Width)
-	{
-		this.x = _x ;
+
+	Rectangle(double _x, double _y, double _Height, double _Width) {
+		this.x = _x;
 		this.y = _y;
 		this.Height = _Height;
-		this.Width = _Width;	
+		this.Width = _Width;
 	}
 
-	double getWidth()
-    {
-	    return this.Width;
-    }
+	double getWidth() {
+		return this.Width;
+	}
 
-	double getHeight()
-    {
-	    return this.Height;
-    }
-    
-    public String toString()
-    {
-    	return "java.Retangle[x=" + this.x + ",y=" + this.y + ",width=" + this.Width + ",height" + this.Height +"]" ;  
-    } 
+	double getHeight() {
+		return this.Height;
+	}
+
+	public String toString() {
+		return "java.Retangle[x=" + this.x + ",y=" + this.y + ",width=" + this.Width + ",height" + this.Height + "]";
+	}
 }
 
-public class RectangleTester 
-{
-	public static void main(String[] args)
-	{
+public class RectangleTester {
+	public static void main(String[] args) {
+
+		Rectangle a1 = new Rectangle(15, 25, 18, 57);
+		Rectangle a2 = new Rectangle(0, 12, 47, 60);
 		
-		Rectangle a1 = new Rectangle(15,25,18,57);
-		Rectangle a2 = new Rectangle(0,12,47,60);
-		
+		//其實程式會自動幫你加上.toString(), 試試看!
 		System.out.println(a1.toString());
-		System.out.println("area="+a1.getWidth()*a1.getHeight());
-		System.out.println("Perimeter="+(a1.getWidth()+a1.getHeight())*2+"\r\n");
+		
+		System.out.println("area=" + a1.getWidth() * a1.getHeight());
+		System.out.println("Perimeter=" + (a1.getWidth() + a1.getHeight()) * 2 + "\r\n");
 		System.out.println(a2.toString());
-		System.out.println("area="+a2.getWidth()*a2.getHeight());
-		System.out.println("Perimeter="+(a2.getWidth()+a2.getHeight())*2 +"\r\n");
+		System.out.println("area=" + a2.getWidth() * a2.getHeight());
+		System.out.println("Perimeter=" + (a2.getWidth() + a2.getHeight()) * 2 + "\r\n");
 		System.out.println("End of Output!");
-			
-	}	
+
+	}
 }
